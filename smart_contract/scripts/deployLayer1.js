cc8ef545e1668d4f280f6fde5cb82cd69ce98b93
3R11K <erikbatista1222@gmail.com>
2024-03-23 11:19:43 -0300

    correção contract

--- smart_contract/scripts/deployLayer1.js
+++ smart_contract/scripts/deployLayer1.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 async function main(){
     const [deployer] = await ethers.getSigners();
     console.log("Deploying contracts with the account:", deployer.address);
-    const contract = await ethers.getContractFactory("BaoTrack");
+    const contract = await ethers.getContractFactory("BaoEnergy");
 
     const bao = await contract.deploy();
 
